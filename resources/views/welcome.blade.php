0f3316b0113088f05ce862de7517752b1681aedb
Nursyafika08 <nursyafika@mhs.unsoed.ac.id>
2026-09-17 23:20:50 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemrograman Web II</title>
+</head>
+<body>
+    <h1>Praktikum Pemrograman Web II</h1>
+    <p>Nama: Nursyafika</p>
+    <p>NIM: H1H024023</p>
+    <p>Program Studi: Teknik Komputer</p>
+</body>
+</html>
